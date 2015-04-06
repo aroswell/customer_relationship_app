c7af4c13cd5b67c24db6499633b5a1c8d644d3f5
aroswell <aroswell@gmail.com>
2015-04-06 14:14:34 -0400

    beginning CRM

---
+++ crm.rb
@@ -0,0 +1,10 @@
+#file holds main program and main class
+
+puts "[1] Add a new contact"
+puts "[2] Modify an existing contact"
+puts "[3] Display All contacts"
+puts "[4] Display a contact"
+puts "[5] Display Attribute of a contact"
+puts "[6] Delete a contact"
+puts "[7] Exit"
+print "\nPlease enter your selection (between 1 to 7):"
